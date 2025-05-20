62c4fb2cdefc092d340306cd92f4ca0eb41b9716
punkharb <ppbthunder@gmail.com>
2025-05-20 21:18:24 +0700

    Add: profile page

---
+++ frontend/src/components/modules/profilepatient.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import DetailPatient from "@/components/card/detail-patient";
+import ListBook from "@/components/list/list-book";
+
+const ProfilePatient = () => {
+  return (
+    <div>
+      <div className="lg:w-200 py-20 ">
+        <DetailPatient />
+      </div>
+      <p className="text-3xl">Your Appointments</p>
+      <ListBook Available={false} />
+    </div>
+  );
+};
+
+export default ProfilePatient;
